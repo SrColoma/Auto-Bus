1da719d61741b2026fbe13e245e20735073d2e34
Alfred Coloma <SrColoma.oficial@gmail.com>
2024-08-21 22:43:19 -0500

    mensajes de errores para debug

--- addons/auto-bus/src/SignalNode.gd
+++ addons/auto-bus/src/SignalNode.gd
@@ -1,14 +1,18 @@
 @icon("icons/SignalNode.svg")
 class_name SignalNode
 extends Node
+signal signal_registered(signal_name)
 
 ## used to emit the signal
 func emit(args = []):
 	get_tree().emit_signal(name,args)
-
+	#print("emitiendo desde " + name)
 
 func _notification(what):
 	match what:
 		NOTIFICATION_READY:
 			if not get_tree().has_user_signal(name):
-				get_tree().add_user_signal(name,[])
+				get_tree().add_user_signal(name, [])
+				emit_signal("signal_registered", name)
+			else:
+				printerr("repeated signal")
--- addons/auto-bus/src/SignalReceptor.gd
+++ addons/auto-bus/src/SignalReceptor.gd
@@ -11,7 +11,12 @@ signal react(args)
 func _notification(what):
 	match what:
 		NOTIFICATION_READY:
-			if get_tree().has_user_signal(react_to):
-				get_tree().connect(react_to,func(args):
-					react.emit(args)
-				)
+			call_deferred("_connect_to_signal")
+
+func _connect_to_signal():
+	if get_tree().has_user_signal(react_to):
+		get_tree().connect(react_to, func(args):
+			react.emit(args)
+		)
+	else:
+		printerr("no existe senial con nombre " + react_to)
